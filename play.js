c48e17b4018e9aeebe73724a7fbef2da5dd846ae
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2022-12-14 10:47:24 -0600

    export function

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
-const net = require("net");
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-  
-  conn.on("connect", () => {
-    conn.write("Client says hello!");
-  });
+const { connect } = require("./client.js");
 
 
-conn.setEncoding("utf8");
-
-return conn;
-
-};
 
 console.log("Connection ...");
 connect();
